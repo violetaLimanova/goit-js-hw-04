27666b5b02b952f67189c2244cc72f1ff72123ec
violeta <limanovavioletta@gmail.com>
2026-09-12 16:51:45 +0300

    all changes

--- js/task-3.js
+++ js/task-3.js
@@ -13,7 +13,7 @@ const profile = {
   },
 
   getInfo() {
-    return `${this.username} has ${this.playTime} active hours`;
+    return `${this.username} has ${this.playTime} active hours!`;
   },
 };
 
